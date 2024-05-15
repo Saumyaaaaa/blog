a5a58334f42438d2ec5333a123da62c7ffc72fa3
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-05-15 16:11:10 +0545

    Set up routing with Layout component and pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import ErrorPost from "./pages/ErrorPost";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import UserProfile from "./pages/UserProfile";
+import CreatePosts from "./pages/CreatePosts";
+import EditPosts from "./pages/EditPosts";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPost />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "posts/:id",
+        element: <PostDetail />,
+      },
+      {
+        path: "profile/:id",
+        element: <UserProfile />,
+      },
+      {
+        path: "create",
+        element: <CreatePosts />,
+      },
+      {
+        path: "posts/:id/edit",
+        element: <EditPosts />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>hello</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
