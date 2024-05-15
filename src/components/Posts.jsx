0ddd46d0f36dbfb71d25559d3f718322d49ce621
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-05-15 20:52:27 +0545

    Add Posts component with dummy data and PostItem integration

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -43,8 +43,18 @@ const Posts = () => {
   const [posts, setPosts] = useState(DUMMY_POSTS);
   return (
     <section className="posts">
-      {posts.map(() => 
-        <PostItem />
+      {posts.map(
+        ({ id, thumbnail, category, title, desc, authorID }) => (
+          <PostItem
+            key={id}
+            postID={id}
+            thumbnail={thumbnail}
+            category={category}
+            title={title}
+            description={desc}
+            authorID={authorID}
+          />
+        )
       )}
     </section>
   );
