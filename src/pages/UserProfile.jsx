cd8ce1bf09832b2949247d1febbef45251006208
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-05-16 08:06:16 +0545

    added style for form

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -5,7 +5,9 @@ import { FaEdit } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa";
 
 const UserProfile = () => {
-    const[avatar,setAvatar]=useState('')
+    const[avatar,setAvatar]=useState(Avatar)
+    const[name,setName]=useState('')
+    const[email,setEmail]=useState('')
   return (
     <section className="profile">
       <div className="container profile__container">
@@ -15,7 +17,7 @@ const UserProfile = () => {
         <div className="profile__details">
           <div className="avatar__wrapper">
             <div className="profile__avatar">
-              <img src={Avatar} alt="" />
+              <img src={avatar} alt="" />
             </div>
             {/* Form to update avatar */}
             <form className="avatar__form">
@@ -35,6 +37,13 @@ const UserProfile = () => {
             </button>
           </div>
           <h1>Christopher Columbus</h1>
+          {/* form to update details */}
+          <form className="form profile__form">
+            <p className="form__error-message">This is an error message</p>
+            <input type="text" placeholder='Tomy Potter' value={name}  onChange={e=>setName(e.target.value)}/>
+            <input type="email" placeholder='ram@gmail.com' value={email}  onChange={e=>setEmail(e.target.value)}/>
+            <button type='submit' className='btn primary'>Update details</button>
+          </form>
         </div>
       </div>
     </section>
